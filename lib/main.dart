aedeba06e2cc19bbeeead0df0a40c29016da75c0
Adi <bhattadi@umich.edu>
2020-05-13 17:45:47 -0400

    Finished Part 2 of creating the flutter tutorial on Favoriting Startup names

--- lib/main.dart
+++ lib/main.dart
@@ -11,51 +11,119 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SunRise Name Generator',
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class RandomWords extends StatefulWidget
+{
   @override
   RandomWordsState createState() => RandomWordsState();
 }
 
-class RandomWordsState extends State<RandomWords> {
-  @override
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+class RandomWordsState extends State<RandomWords>
+{
+    // TODO Add build() method
+    final List<WordPair> _suggestions = <WordPair>[];
+    final Set<WordPair> _saved = Set<WordPair>();
+    final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
 
-  Widget build(BuildContext context) {
+    @override
+    void _pushSaved()
+    {
+      Navigator.of(context).push(
+        MaterialPageRoute<void>(
+          builder: (BuildContext context)
+          {
+            final Iterable<ListTile> tiles = _saved.map(
+              (WordPair pair) 
+              {
+                return ListTile(
+                  title: Text
+                  (
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+            );
+            final List<Widget> divided = ListTile
+              .divideTiles(
+                context: context,
+                tiles: tiles,
+              )
+              .toList();
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided),
+            );
+          }
+        )
+      );
+    }
+
+    Widget build(BuildContext context)
+    {
       return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
+        appBar: AppBar(
+          title: Text('Startup Name Generator'),
+          actions: <Widget>[IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),]
+        ),
+        body: _buildSuggestions(),
+      );
+    }
 
-  Widget _buildRow(WordPair pair) {
-  return ListTile(
-    title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ),
-  );
-  }
+    Widget _buildSuggestions()
+    {
+      return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i)
+        {
+          if(i.isOdd) return Divider();
 
-  Widget _buildSuggestions() {
-  return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return Divider(); /*2*/
-
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          final index = i ~/ 2;
+          if(index >= _suggestions.length)
+          {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
         }
-        return _buildRow(_suggestions[index]);
-      });
-  }
+      );
+    }
+
+    Widget _buildRow(WordPair pair)
+    {
+      final bool alreadySaved = _saved.contains(pair);
+      return ListTile(
+        title: Text(
+          pair.asPascalCase, 
+          style: _biggerFont,
+        ),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: ()
+        {
+          setState(() {
+            if(alreadySaved)
+            {
+              _saved.remove(pair);
+            }
+            else
+            {
+              _saved.add(pair);
+            }
+          });
+        },
+      );
+    }
 }
+
