056825ff156aa81aa97f7965260914928961a25b
alagiset <alagiset@umich.edu>
2020-05-15 00:40:14 -0400

    have an interactive calendar, needs to be improved still

--- lib/booking.dart
+++ lib/booking.dart
@@ -1,8 +1,55 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
+//import 'package:some_calendar/some_calendar.dart';
+//import 'package:intl/intl.dart';
+//import 'package:intl/date_symbol_data_local.dart';
 
-class BookingPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import 'package:jiffy/jiffy.dart';
+import 'package:some_calendar/some_calendar.dart';
+
+
+class BookingPage extends StatefulWidget {
+  @override
+  _BookingPageState createState() => _BookingPageState();
+}
+
+class _BookingPageState extends State<BookingPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    List<DateTime> selectedDates = List();
+
+    @override
+    void initState() {
+      initializeDateFormatting();
+      Intl.systemLocale = 'en_En'; // to change the calendar format based on localization
+      super.initState();
+    }
+    return MaterialApp (
+      home: Scaffold(
+        body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(18),
+          child: SomeCalendar(
+            primaryColor: Color(0xff5833A5),
+            mode: SomeMode.Range,
+            isWithoutDialog: true,
+            selectedDates: selectedDates,
+            startDate: Jiffy().subtract(years: 3),
+            lastDate: Jiffy().add(months: 9),
+            done: (date) {
+              setState(() {
+                selectedDates = date;
+                //showSnackbar(selectedDates.toString());
+              });
+            },
+          ),
+        )
+        ),
+        ),
+      );
   }
 }
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutterapp/login.dart';
 
 import 'package:flutterapp/main.dart';
 
