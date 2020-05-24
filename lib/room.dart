869a9bdd4c680223818f94679da5ea81003b72a8
Adi <bhattadi@umich.edu>
2020-05-24 01:01:22 -0400

    Temporary fix to having a database of all the days in a year. We only have 1 dummy room in each day in the database since empty lists are not allowed in the database due to a null valued error

--- lib/room.dart
+++ lib/room.dart
@@ -1,14 +1,9 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'dart:convert';
 
 class Day{
-  List<Room> rooms = new List(1);
+  List<Room> rooms = new List<Room>(1);
   Day(this.rooms);
-
-  toJson() {
-    return {
-      "rooms" : rooms
-    };
-  }
 }
 
 class Room{
@@ -17,14 +12,34 @@ class Room{
   int checkout = 2;
 
   Room(this.isAvailable, this.checkin, this.checkout);
+
+  toJson() =>
+  {
+    "isAvailable" : isAvailable,
+    "checkin" : checkin,
+    "checkout" : checkout,
+  };
 }
 
 void setUpCalendar(){
-  List<Day> calendar = new List(364);
+  Room r1 = Room(true, 1, 1);
+  Room r2 = Room(true, 1, 1);
+  Room r3 = Room(true, 1, 1);
+  // Day a = Day([r1, r2, r3]);
+  // Day b = Day([r2, r1, r3]);
+  
+  // List<Day> calendar = [a, b];
+  List<Day> calendar = List<Day>(365);
+
   final FirebaseDatabase _database = FirebaseDatabase.instance;
 
   for(int i = 0; i < calendar.length; ++i)
   {
-   _database.reference().child("calendar").set(calendar[i].toJson());
+    List<Room> rooms = [Room(true, 0, 0)];
+    calendar[i] = Day(rooms);
+    for(int j = 0; j < 1; ++j)
+    {
+      _database.reference().child("calendar").child("Days").child(i.toString()).child("Room Numbers").child(j.toString()).update(calendar[i].rooms[j].toJson());
+    }
   }
 }
